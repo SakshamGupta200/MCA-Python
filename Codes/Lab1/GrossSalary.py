5d80325c3adec26d651383bda7957c4186da42bf
Saksham Gupta <121724970+SakshamGupta200@users.noreply.github.com>
2023-11-01 07:51:13 +0530

    commit

--- Codes/Lab1/GrossSalary.py
+++ Codes/Lab1/GrossSalary.py
@@ -1,3 +1,10 @@
-"""
+bs = float(input("Enter the Basic Salary :"))
+if bs < 15000:
+    hra = bs * 0.1
+    da = bs * 0.9
+else:
+    hra = 5000
+    da = bs * 0.98
 
-"""
+gs = bs + hra + da
+print("Gross Salary Rs :", gs)
