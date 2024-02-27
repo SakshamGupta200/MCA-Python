9077aaccdd78b0ffe4d9178fdb2d98734c61ffc4
SakshamGupta200 <121724970+SakshamGupta200@users.noreply.github.com>
2024-02-27 15:50:34 +0530

    commit

---
+++ Skillsforll/test-1.py
@@ -0,0 +1,22 @@
+"""def function_1(a):
+    return None
+
+
+def function_2(a):
+    return function_1(a) * function_1(a)
+
+
+print(function_2(2))"""
+
+# print(1 // 2)
+
+"""def func(a, b):
+    return b ** a
+
+
+print(func(b=2, 2))"""
+
+z = 0
+y = 10
+x = y < z and z > y or y < z and z < y
+print(x)
